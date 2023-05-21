cb11b2591008f3fff8ddf217eb6c93aa7a9896b3
Sotnik0xFF <sotnikoff1987@gmail.com>
2023-05-21 23:45:53 +0500

    Add [SetUp] Init method to CalculatorTests.

--- Module16.Tests/CalculatorTests.cs
+++ Module16.Tests/CalculatorTests.cs
@@ -3,41 +3,44 @@ using NUnit.Framework;
 
 namespace Module16.Tests;
 
-[TestFixture]
+[TestFixture, Parallelizable]
 public class CalculatorTests
 {
+    private Calculator? _calculator;
+
+    [SetUp]
+    public void Init()
+    {
+        _calculator = new Calculator();
+    }
+
     [Test]
     public void Additional_MustReturnCorrectValue()
     {
-        Calculator calculator = new Calculator();
-        Assert.True(calculator.Additional(10, 20) == 30);
+        Assert.True(_calculator!.Additional(10, 20) == 30);
     }
 
     [Test]
     public void Subtraction_MustReturnCorrectValue()
     {
-        Calculator calculator = new Calculator();
-        Assert.True(calculator.Subtraction(3, 2) == 1);
+        Assert.True(_calculator!.Subtraction(3, 2) == 1);
     }
 
     [Test]
     public void Multiplication_MustReturnCorrectValue()
     {
-        Calculator calculator = new Calculator();
-        Assert.True(calculator.Multiplication(3, 2) == 6);
+        Assert.True(_calculator!.Multiplication(3, 2) == 6);
     }
 
     [Test]
     public void Division_MustReturnCorrectValue()
     {
-        Calculator calculator = new Calculator();
-        Assert.True(calculator.Division(10, 2) == 5);
+        Assert.True(_calculator!.Division(10, 2) == 5);
     }
 
     [Test]
     public void Division_MustThrowDivideByZeroException()
     {
-        Calculator calculator = new Calculator();
-        Assert.Throws<DivideByZeroException>(() => calculator.Division(10, 0));
+        Assert.Throws<DivideByZeroException>(() => _calculator!.Division(10, 0));
     }
 }
